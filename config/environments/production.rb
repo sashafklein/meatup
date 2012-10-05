9051996322371650f82868638907553da660aa94
Sasha Klein <sashafklein@gmail.com>
2012-10-05 16:00:41 -0700

    Configured production email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,19 @@
 Meatup2::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  # Settings for email in production
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "meatup.in",
+    :user_name            => ENV["GMAIL_USERNAME"],
+    :password             => ENV["GMAIL_PASSWORD"],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
