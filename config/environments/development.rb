d04943e6b8dbdf700227dbfbcdb0a66389f6e79e
Niko Klein <nikoklein2@gmail.com>
2012-10-13 19:50:59 -0700

    From Niko's functioning computer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Meatup2::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-config.action_mailer.default_url_options = { host: "localhost:3000" }
+config.action_mailer.default_url_options = { :host => "localhost:3000" }
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
