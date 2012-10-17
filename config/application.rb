01b00aa13d6b2b9d3bd851d59ee3f3d9bcdcad63
Sasha Klein <sashafklein@gmail.com>
2012-10-16 23:11:20 -0700

    Again for heroku

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,9 @@ module Meatup2
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # To fix a heroku bootstrap load problem
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
