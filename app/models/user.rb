ba4a4eafd935afa166f6f7ec5e4a3221300fa3ef
Sasha Klein <sashafklein@gmail.com>
2012-10-24 16:19:26 -0700

    Commented out non-functional zip test

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,7 @@ class User < ActiveRecord::Base
 
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
-  after_create :zip_test
+  # after_create :zip_test
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
