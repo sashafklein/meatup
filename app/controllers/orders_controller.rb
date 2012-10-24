e4e8d7b35e9518300c250633d3e3a182d59b29d0
Sasha Klein <sashafklein@gmail.com>
2012-10-24 15:32:47 -0700

    Fixed a sign-out bug

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -66,6 +66,31 @@ before_filter :correct_user, only: [:show, :edit, :update, :destroy]
   def pig_order
     @order = Order.new
     @order.lines.build
+    @meat = "Pig"
+
+    @animal_list = Animal.where(:animal_type => @meat)
+    @order_animal = @animal_list.last
+
+    if @order_animal.opening_sale
+      
+      hours_left = (120 - (Time.now.to_i - @order_animal.created_at.to_i)/60)/60
+      mins_left = (120 - (Time.now.to_i - @order_animal.created_at.to_i)/60)%60
+
+      if hours_left == 1 
+        time_left = "1 hour and #{mins_left} minutes"
+      elsif hours_left > 1
+        time_left = "#{hours_left} hours"
+      else
+        time_left = "#{mins_left} minutes"
+      end
+
+      flash[:notice] = "Opening Sale on select cuts for the next #{time_left}!"
+
+    elsif @order_animal.final_sale
+
+      flash[:notice] = "Final Sale on all cuts until #{@order_animal.name} is sold out!"
+
+    end 
 
     respond_to do |format|
       format.html # new.html.erb
@@ -76,6 +101,31 @@ before_filter :correct_user, only: [:show, :edit, :update, :destroy]
   def lamb_order
     @order = Order.new
     @order.lines.build
+    @meat = "Lamb"
+
+    @animal_list = Animal.where(:animal_type => @meat)
+    @order_animal = @animal_list.last
+
+    if @order_animal.opening_sale
+      
+      hours_left = (120 - (Time.now.to_i - @order_animal.created_at.to_i)/60)/60
+      mins_left = (120 - (Time.now.to_i - @order_animal.created_at.to_i)/60)%60
+
+      if hours_left == 1 
+        time_left = "1 hour and #{mins_left} minutes"
+      elsif hours_left > 1
+        time_left = "#{hours_left} hours"
+      else
+        time_left = "#{mins_left} minutes"
+      end
+
+      flash[:notice] = "Opening Sale on select cuts for the next #{time_left}!"
+
+    elsif @order_animal.final_sale
+
+      flash[:notice] = "Final Sale on all cuts until #{@order_animal.name} is sold out!"
+
+    end 
 
     respond_to do |format|
       format.html # new.html.erb
@@ -86,6 +136,31 @@ before_filter :correct_user, only: [:show, :edit, :update, :destroy]
   def goat_order
     @order = Order.new
     @order.lines.build
+    @meat = "Goat"
+
+    @animal_list = Animal.where(:animal_type => @meat)
+    @order_animal = @animal_list.last
+
+    if @order_animal.opening_sale
+      
+      hours_left = (120 - (Time.now.to_i - @order_animal.created_at.to_i)/60)/60
+      mins_left = (120 - (Time.now.to_i - @order_animal.created_at.to_i)/60)%60
+
+      if hours_left == 1 
+        time_left = "1 hour and #{mins_left} minutes"
+      elsif hours_left > 1
+        time_left = "#{hours_left} hours"
+      else
+        time_left = "#{mins_left} minutes"
+      end
+
+      flash[:notice] = "Opening Sale on select cuts for the next #{time_left}!"
+
+    elsif @order_animal.final_sale
+
+      flash[:notice] = "Final Sale on all cuts until #{@order_animal.name} is sold out!"
+
+    end 
 
     respond_to do |format|
       format.html # new.html.erb
