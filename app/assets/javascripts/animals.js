c122b0570d3e6ac8b7c6976bd08ddd6bb319763f
Sasha Klein <sashafklein@gmail.com>
2012-12-11 17:28:59 -0800

    Removed redundant .js and .css.scss files

--- app/assets/javascripts/animals.js
+++
@@ -1,9 +0,0 @@
-jQuery ->
-Morris.Donut({
-  element: 'salesforce',
-  data: [
-    {label: "Download Sales", value: 12},
-    {label: "In-Store Sales", value: 30},
-    {label: "Mail-Order Sales", value: 20}
-  ]
-});
