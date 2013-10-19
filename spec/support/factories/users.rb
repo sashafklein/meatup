3fa622b684d0a94eb4b9983545c0211e6ac0b4df
Sasha Klein <sashafklein@gmail.com>
2013-10-18 22:02:49 -0700

    Removed user.rb in lib, which looked to have been creating User Class issues

--- spec/support/factories/users.rb
+++ spec/support/factories/users.rb
@@ -1,32 +1,32 @@
-FactoryGirl.define do
+# FactoryGirl.define do
 
-  factory :user, class: User do
+#   factory :user, class: User do
     
-    name     "Basic User"
-    email    "basic@emailclient.com"
-    password "password"
-    password_confirmation "password"
-    zip 94114
+#     name     "Basic User"
+#     email    "basic@emailclient.com"
+#     password "password"
+#     password_confirmation "password"
+#     zip 94114
 
-    factory :sasha do
-      name "Sasha Klein"
-      email "sasha@emailclient.com"
-      admin true
-      beta false
-    end
+#     factory :sasha do
+#       name "Sasha Klein"
+#       email "sasha@emailclient.com"
+#       admin true
+#       beta false
+#     end
 
-    factory :nonbeta do
-    	name     "NonBeta User"
-      email    "blocked@emailclient.com"
-      admin false
-      beta true
-    end
+#     factory :nonbeta do
+#     	name     "NonBeta User"
+#       email    "blocked@emailclient.com"
+#       admin false
+#       beta true
+#     end
 
-    factory :beta do
-    	name     "Beta User"
-      email    "user@emailclient.com"
-      admin false
-      beta true
-    end
-  end
-end
+#     factory :beta do
+#     	name     "Beta User"
+#       email    "user@emailclient.com"
+#       admin false
+#       beta true
+#     end
+#   end
+# end
